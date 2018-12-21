7d570762bace2317e75ac78d86078ab20d790c14
Piotr Kozbial <piotr.jan.kozbial@gmail.com>
2018-12-21 20:20:27 +0100

    m

--- src/clj/gamebase/root_page.clj
+++ src/clj/gamebase/root_page.clj
@@ -29,8 +29,7 @@
 
             ;;[:script {:type "text/javascript"} "app.system.go();"]
             [:script
-             ;;(str "function setup() { app.system.go(); " main "();\n }\n")
-             (str "function setup() { app.system.go(); }\n")
+             (str "function setup() { app.system.go(); " main "();\n }\n")
 
              "function draw()          {
  gamebase.events.callback(\"draw\"); }\n"
